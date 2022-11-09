640e587799a05e19b067c43e6630b92457ec7362
Mehmet Tekin <tekimmehmet@hotmail.com>
2022-11-09 23:15:33 +0300

    MyTextEditComponenti tasarlandı

---
+++ TekinTecnology.StudentTrackingProject.UI.Win/UserControls/Controls/MyTextEdit.cs
@@ -0,0 +1,19 @@
+﻿using DevExpress.XtraEditors;
+using System.ComponentModel;
+using System.Drawing;
+using TekinTecnology.StudentTrackingProject.UI.Win.Interfaces;
+
+namespace TekinTecnology.StudentTrackingProject.UI.Win.UserControls.Controls
+{
+    public class MyTextEdit:TextEdit,IStatusBarAciklama
+    {
+        [ToolboxItem(true)]
+        public MyTextEdit()
+        {
+            Properties.AppearanceFocused.BackColor = Color.LightCyan;
+            Properties.MaxLength = 50;
+        }
+        public override bool EnterMoveNextControl { get; set; } = true;
+        public string StatusBarAciklama { get ; set; }
+    }
+}
